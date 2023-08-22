ba1ce55adba8f6311f1f47b453568d0c773c7798
Felipe Bona <felipechiodinibona@hotmail.com>
2023-08-21 22:48:33 -0300

    Ajsutes

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,12 +5,25 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-// use Tymon\JWTAuth\Contracts\JWTSubject;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use HasFactory, Notifiable;
 
+    protected $filafillable = [
+        'name',
+        'email'
+    ];
 
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 
 }
