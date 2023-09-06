eda2adbae4150038bad15b170596156f3389ef82
Felipe Bona <felipechiodinibona@hotmail.com>
2023-09-05 22:27:40 -0300

    doing main site

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -10,25 +10,19 @@ use Illuminate\Support\Facades\Route;
 
 class RouteServiceProvider extends ServiceProvider
 {
-    /**
-     * The path to your application's "home" route.
-     *
-     * Typically, users are redirected here after authentication.
-     *
-     * @var string
-     */
     public const HOME = '/home';
 
-    /**
-     * Define your route model bindings, pattern filters, and other route configuration.
-     */
     public function boot(): void
     {
         RateLimiter::for('api', function (Request $request) {
             return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
         });
 
-        $this->routes(function () {
+        Route::middleware('web')
+            ->namespace('App\Http\Controllers\Company')
+            ->group(base_path('routes/web.php'));
+
+        Route::prefix('api', function () {
             Route::middleware('api')
                 ->namespace('App\Http\Controllers')
                 ->group(base_path('routes/api.php'));
