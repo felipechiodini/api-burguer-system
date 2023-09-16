bacb4f77e4550a8dd1054042ef884446c4337bf9
Felipe Bona <felipechiodinibona@hotmail.com>
2023-09-16 13:30:34 -0300

    Ajusts

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -42,4 +42,19 @@
     <img src="{{ asset('images/cellphone.png') }}" alt="cellphone">
     <a href="/criar-conta">Criar Conta Grátis</a>
 </section>
+
+<section>
+    <h1>Facilidade na hora de gerenciar seus pedidos</h1>
+</section>
+
+<section>
+    <h1>Dúvidas sobre a Burguer System</h1>
+</section>
+
+<section>
+    <h1>Acompanhe nas redes sociais</h1>
+    <i class="fas fa-linkedin"></i>
+    <i class="fas fa-youtube"></i>
+    <i class="fas fa-twityet"></i>
+</section>
 @endsection
