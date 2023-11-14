8961bc0ac961ba7b40b333c220880e3f6584a968
Felipe Chiodini Bona <felipechiodinibona@hotmail.com>
2023-11-13 22:37:59 -0300

    ajsuts

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,17 +2,35 @@
 
 namespace Tests\Feature;
 
-// use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Models\User;
+use App\Models\UserStore;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Hash;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     */
+    use RefreshDatabase;
+
     public function test_the_application_returns_a_successful_response(): void
     {
-        $response = $this->get('/');
+
+        // User::query()->create([
+        //     'name' => 'dddddddddddddddddd',
+        //     'email' => 'felipechiodinibona@hotmail.com',
+        //     'cellphone' => '47999097073',
+        //     'password' => Hash::make('132567'),
+        //     'root' => true
+        // ]);
+
+        // dd(User::query()->get());
+        // UserStore::query()->create([
+        //     'name' => 'vais se fuder',
+        //     'slug' => 'fodase'
+        // ]);
+        // dd(UserStore::query()->get());
+        $response = $this->get('/delivery/bona/store');
 
         $response->assertStatus(200);
     }
